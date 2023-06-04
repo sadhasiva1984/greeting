16cde19f8c42d3954c9d8f99d6f500bd47177412
Sadhasivasubramanian Hariharasubramanian <sadha@Sadhas-MBP.local>
2023-06-04 11:46:06 +0530

    update go.mod

--- greeting.go
+++ greeting.go
@@ -1,11 +1,14 @@
+//Package greeting enables greeting from different language
 package greeting
 
 import "fmt"
 
+// Hello Prints Hello
 func Hello() {
 	fmt.Println("Hello!")
 }
 
+// Hi Prints Hi
 func Hi() {
 	fmt.Println("Hi!")
 }
